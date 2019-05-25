f62257d2b59ebfdb4de9afb9d0c6e3f09fb77e84
Brzhk <berzehk@gmail.com>
2019-05-26 01:08:04 +0200

    fixes foobar api exposition

--- src/main/java/wtf/brzhk/simpleapp/DemoApplication.java
+++ src/main/java/wtf/brzhk/simpleapp/DemoApplication.java
@@ -5,6 +5,7 @@ import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -13,6 +14,7 @@ import java.util.Map;
 import static java.util.Collections.singletonMap;
 
 @SpringBootApplication
+@Controller
 public class DemoApplication {
 
     @Autowired
